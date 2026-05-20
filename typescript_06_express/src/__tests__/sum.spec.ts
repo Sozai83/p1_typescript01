d479b8933754ce4d69ea4bfc09ee418ea81018e1
Shiori Chiku <chikushi3.227@gmail.com>
2026-05-20 10:38:36 +1000

    gitaction check

--- typescript_06_express/src/__tests__/sum.spec.ts
+++ typescript_06_express/src/__tests__/sum.spec.ts
@@ -12,6 +12,6 @@ describe("sum function", () => {
     });
 
     it("should add two negative numbers correctly", () => {
-        expect(sum(-4, -6)).toBe(-9);
+        expect(sum(-4, -6)).toBe(-8);
     });
 });
