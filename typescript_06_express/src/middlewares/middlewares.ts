c3c1bf3a212163a4629c96f2c3f6db68ab94ffc9
Shiori Chiku <chikushi3.227@gmail.com>
2026-05-20 10:57:07 +1000

    test

--- typescript_06_express/src/middlewares/middlewares.ts
+++ typescript_06_express/src/middlewares/middlewares.ts
@@ -3,6 +3,6 @@ import type { RequestHandler } from "express";
 
 export const middleware: RequestHandler = (req, res, next) => {
   res.send("Hello World!");
-  console.log("Middleware response sent - test changed");
+  console.log("Middleware response sent");
   next();
 };
